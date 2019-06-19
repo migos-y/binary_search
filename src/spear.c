20a8f7d3005038ada4129a27cff3b58bae3430ec
迷子迷い子 <meguuri4734121sjh@gmail.com>
2019-06-19 22:09:10 +0900

    spear.cした

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,15 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int count = 0;
+  for(int i = 0;i < n;i++){
+    count += A[i] / x;
+    if(count >= k) return 1; //作れるとき
+  }
+  return 0; //作れないとき
+}
+
 
 int main(){
   int i, lb, ub;
@@ -11,7 +20,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int x = (ub + lb) / 2;
+    if(p(x)){
+      lb = x;
+    }else{
+      ub = x;
+    }
+  }
 
-
+  printf("%d\n", lb);
   return 0;
 }
