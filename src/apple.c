135dcb30fdd2de5e63b3e88b27ee5e2f50a25649
迷子迷い子 <meguuri4734121sjh@gmail.com>
2019-06-19 20:16:15 +0900

    apple.cした

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,21 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){ //mは袋の容量
+  int count = 0;
+  for(int i = 0; i < n;i++){
+    if(A[i] < m){
+      count++;
+    }else if(A[i] % m == 0){
+      count += A[i] / m;
+    }else{
+      count += A[i] / m + 1;
+    }
+
+    if(count > k) return 0;
+  }
+  return 1;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +26,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int m = (lb + ub) / 2;
+    if(p(m)){
+      ub = m;
+    }else{
+      lb = m;
+    }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
