f2abbc2c973fd203fea7e0d6c6afce67f131dd98
迷子迷い子 <meguuri4734121sjh@gmail.com>
2019-06-21 02:38:55 +0900

    works.cした

--- src/works.c
+++ src/works.c
@@ -4,6 +4,27 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int a = 0;
+  int count = 0;
+  for(int i = 0;i < n;i++){
+    if(A[i] > m){
+      return 0;
+    }
+
+    if(a + A[i] > m){
+      a = A[i];
+      count++;
+    }else{
+      a += A[i];
+    }
+  }
+  if(count >= k){
+    return 0;
+  }else{
+    return 1;
+  }
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +33,17 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  ub = 1000000000;
+  lb = 0;
+  while(ub - lb > 1){
+    int m = (ub + lb) / 2;
+    if(p(m)){
+      ub = m;
+    }else{
+      lb = m;
+    }
+  }
 
+  printf("%d", ub);
   return 0;
 }
